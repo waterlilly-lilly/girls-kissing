c4bb19b447af5d2f59b6165012592dd5ba2cf2bc
Lilly Rosaline <lilly.rosaline@waterlilly.dev>
2024-03-30 15:30:39 -0500

    Initial commit

---
+++ common/src/main/java/com/example/examplemod/Constants.java
@@ -0,0 +1,16 @@
+package com.example.examplemod;
+
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+
+public class Constants {
+
+    // This is the mod ID.
+    public static final String MOD_ID = "examplemod";
+
+    // This is the name of the mod.
+    public static final String MOD_NAME = "ExampleMod";
+
+    // This is the logger for the mod. It will automatically have the mod name and show up properly in the log.
+    public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
